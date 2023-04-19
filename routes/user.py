6ba4dbb2ce2d179043bc11b0b773eb51a5000a61
paul479 <pgomez4790@gmail.com>
2023-04-19 11:12:13 -0500

    commit 7 - borrar response y tags de las url

--- routes/user.py
+++ routes/user.py
@@ -9,11 +9,13 @@ from starlette.status import HTTP_204_NO_CONTENT
 user =APIRouter()
 
 
-@user.get('/users',response_model=list[User], tags=["users"])
+#@user.get('/users',response_model=list[User], tags=["users"])
+@user.get('/users')
 def find_all_user():
     return usersEntity(conn.user.find())
 
-@user.post('/users',response_model=User, tags=["users"])
+#@user.post('/users',response_model=User, tags=["users"])
+@user.post('/users')
 def create_user(user: User):
     new_user = dict(user)
     new_user["password"] = sha256_crypt.encrypt(new_user["password"])
@@ -22,17 +24,19 @@ def create_user(user: User):
     user = conn.user.find_one({"_id":id})
     return userEntity(user)
     
-
-@user.get('/users/{id}',response_model=User, tags=["users"])
+#@user.get('/users/{id}',response_model=User, tags=["users"])
+@user.get('/users/{id}')
 def find_user(id: str):
     return userEntity(conn.user.find_one({"_id":ObjectId(id)}))
 
-@user.put('/users/{id}',response_model=User, tags=["users"])
+#@user.put('/users/{id}',response_model=User, tags=["users"])
+@user.put('/users/{id}')
 def update_user(id:str, user: User):
     conn.user.find_one_and_update({"_id":ObjectId(id)}, {"$set": dict(user)})
     return userEntity(conn.user.find_one({"_id":ObjectId(id)}))
 
-@user.delete('/users/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=["users"])
+#@user.delete('/users/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=["users"])
+@user.delete('/users/{id}')
 def delete_user(id: str):
     userEntity(conn.user.find_one_and_delete({"_id":ObjectId(id)}))
     return Response(status_code=HTTP_204_NO_CONTENT)
